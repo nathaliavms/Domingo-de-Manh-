46dae6a7713a160a1265b94d676789f649b827b5
Nathalia Vieira <Silva.Nathalia@ibm.com>
2022-08-29 11:03:17 -0300

    first commit

---
+++ Feira/Entities/Horario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Feira
+{
+    class Horario
+    {
+        public int Horas { get;  private set; }
+        public int Minutos { get; private set; }
+        public int Diferenca { get; private set; }
+
+        public Horario(int horas, int minutos)
+        {
+            Horas = horas;
+            Minutos = minutos;
+        }
+        public void CalculoDeAtraso()
+        {
+            if (Horas < 8 && Minutos < 01 || Horas < 7)
+            {
+                Console.WriteLine("Atraso maximo: " + 0);
+            }
+            else
+            {
+                Diferenca = (Horas - 7) * 60 + Minutos;
+                Console.WriteLine("Atraso maximo: " + Diferenca);
+            }
+        }
+    }   
+}
---
+++ Feira/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Feira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o horário que acordou: ");
+            string[] HorarioInformado = Console.ReadLine().Split(":");
+            var horas = int.Parse(HorarioInformado[0]);
+            var minutos = int.Parse(HorarioInformado[1]);
+
+            Horario Horario = new Horario(horas, minutos);
+            Horario.CalculoDeAtraso();
+        }
+    }
+}
