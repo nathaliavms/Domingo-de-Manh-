c7a8b8a94f38f6ecafce996b21fd19e10e964129
Nathalia Vieira <101611468+nathaliavms@users.noreply.github.com>
2022-08-30 09:49:43 -0300

    Update Horario.cs

--- Feira/Entities/Horario.cs
+++ Feira/Entities/Horario.cs
@@ -1,31 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Feira
 {
-    class Horario
+    class CalcularHoraDaFeira
     {
-        public int Horas { get;  private set; }
-        public int Minutos { get; private set; }
-        public int Diferenca { get; private set; }
-
-        public Horario(int horas, int minutos)
+        private int Horas { get; set; }
+        private int Minutos { get; set; }
+        public CalcularHoraDaFeira(int horas, int minutos)
         {
             Horas = horas;
             Minutos = minutos;
         }
-        public void CalculoDeAtraso()
+        private int CalculoDeAtraso()
         {
+            int diferença;
             if (Horas < 8 && Minutos < 01 || Horas < 7)
             {
-                Console.WriteLine("Atraso maximo: " + 0);
+                return 0;
             }
             else
             {
-                Diferenca = (Horas - 7) * 60 + Minutos;
-                Console.WriteLine("Atraso maximo: " + Diferenca);
+                diferença = (Horas - 7) * 60 + Minutos;
+                return  + diferença;
             }
+        }
+        public override string ToString()
+        {
+            return "Atraso maximo: " + CalculoDeAtraso().ToString();     
         }
     }   
 }
